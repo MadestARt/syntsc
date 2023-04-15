fd296d861fd89df5b42d76e3c85bd584d340e310
Владимиров Артём <asvladimi1@www.mts.ru>
2023-04-15 15:08:24 +0300

    made  stable empty spring version

---
+++ src/main/java/com/asvladimirov/syntsc/SyntscApplication.java
@@ -0,0 +1,13 @@
+package com.asvladimirov.syntsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyntscApplication.class, args);
+    }
+
+}
